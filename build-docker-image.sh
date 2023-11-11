8e095e2b1c70ccd334e5ef90f156a29534627c8e
Freek Dijkstra <freek@macfreek.nl>
2023-11-11 12:42:05 +0100

    Add example docker run build to script

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 cp ~/.ssh/id_rsa.pub allowedcontainerkey.pub
 docker build -t debian-bare-ansible .
+
+# Pick one based on what works for you:
+# docker run -d --privileged -p 2022:22 debian-bare-ansible
+# docker run -d -p 2022:22 -v /sys/fs/cgroup/dockerd.scope:/sys/fs/cgroup:rw debian-bare-ansible
